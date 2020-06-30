9f4fc47a8c0b576a5c2d43044c5da0300c2f9fa4
Muaykhao86 <waveflow18@gmail.com>
2020-06-30 15:53:39 +0100

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,67 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { graphql, Link } from "gatsby"
+import styled from "styled-components"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  `
+
+// gatsby knows this is graphql and will attach the query to our component and export both
+export const query = graphql`
+query {
+  allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC})  {
+    edges {
+      node {
+        id
+        frontmatter {
+          date
+          description
+          title
+        }
+        fields{
+          slug
+        }
+        excerpt
+      }
+    }
+    totalCount
+  }
+}
+`;
+
+
+export default ({data}) => {
+console.log(data);
+return (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <div className="">
+      <h1>My Thoughts</h1>
+      <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+    </div>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    {data.allMarkdownRemark.edges.map(({node}) => (
+      <div key={node.id}>
+        <BlogLink to={node.fields.slug}>
+        <BlogTitle>{node.frontmatter.title} - {node.frontmatter.date}</BlogTitle>
+        <p>{node.excerpt}</p>
+        </BlogLink>
+      </div>
+    ))}
+    <BlogLink to="/page-2/">Go to page 2</BlogLink> <br />
+    <BlogLink to="/page-3/">Go to page 3</BlogLink> <br />
+    <BlogLink to="/using-typescript/">Go to "Using TypeScript"</BlogLink>
   </Layout>
-)
+)}
 
-export default IndexPage
+
+
+
